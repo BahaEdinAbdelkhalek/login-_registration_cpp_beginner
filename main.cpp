d75321ff62ac678d349f8783adc455a879e5b5ea
BahaEdinAbdelkhalek <bahabahabaha2482003@gmail.com>
2024-12-15 22:33:36 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main(){
+  char question;
+  cout << "Welcome to the quthentication process !" << endl;
+  cout << "Are you signed up (y/n) ?? : ";
+cin>>question;
+  cout<<endl;
+
+  
+  return 0;
+}
